d8d7b6107d3b38d60a22e26e7f583ada7e79f2c5
Daniel_eth <danielvigo06@gmail.com>
2025-08-24 17:57:18 +0200

    Upgraded chats and increased chats security

--- src/MessBlockChats.sol
+++ src/MessBlockChats.sol
@@ -4,63 +4,44 @@ pragma solidity ^0.8.7;
 contract MessBlockChats {
 
 /// @notice Thrown when a user attempts to create a chat with someone already in their chat list.
-error UserAlreadyInAChat(); 
-
-/// @notice Thrown when a user attempts to delete a chat that does not exist.
-error UserIsNotInAChat(); 
+error UserIsAlreadyInAChat(address user); 
 
 /// @notice Thrown when a user attempts to create a chat with themselves.
 error CantCreateAChatWithHimself();
 
+/// @notice Thrown when a user attempts to delete a chat that does not exist.
+error UserIsNotInAChat(address user); 
+
 /// @notice Thrown when a user attempts to send a message to themselves.
 error CantSendMessageToHimself();
 
-/// @notice Thrown when a user attempts to modify a message in a single chat but fails.
-error CantModifyTheMessage(); 
-
-/// @notice Thrown when a user attempts to delete a message in a single chat but fails.
-error CantDeleteTheMessage();
-
-/// @notice Thrown when a user cannot delete a chat.
-error UserCantDeleteTheChat();
-
-/// @notice Defines messages within a user chat.
-struct UserMessagges{
+struct ChatMessage {
+address from; 
 string message; 
-address messageSender; 
+uint256 timestamp;
 }
 
+mapping(address => mapping(address => bool)) internal isInAChat;
 /// @notice Mapping of a user to their chats.
 mapping(address => address[])internal addressToChats; 
 
-/// @notice Mapping of users to their messages with other users.
-mapping(address => mapping(address => UserMessagges[]))internal addressToUserMessagess; 
+mapping(bytes => ChatMessage[]) internal messages;	
 
-/// @notice Ensures a user cannot create a chat with someone already in their chat list.
-/// @param newUser The address of the new chat user.
-modifier isNotTheUserAlreadyInAChat(address newUser) {
-address[] memory usersChats = addressToChats[msg.sender]; 
-for(uint256 i = 0; i < usersChats.length; i++){
-	if(newUser == usersChats[i]){
-		revert UserAlreadyInAChat(); 
-	}
+mapping(address => mapping(address => bytes)) public chatKey; 
+
+
+/// @notice Ensures a user is already in a chat before interacting.
+/// @param user The address of the chat participant.
+modifier userIsNotInAChat(address user){
+if(isInAChat[msg.sender][user]){
+	revert UserIsAlreadyInAChat(user); 
 }
 _;
 }
 
-
-/// @notice Ensures a user is already in a chat before interacting.
-/// @param deletedUser The address of the chat participant.
-modifier isTheUserAlreadyInAChat(address deletedUser){
-bool isInAchat = false; 
-for(uint256 i = 0; i < addressToChats[msg.sender].length; i++){
-	if(addressToChats[msg.sender][i] == deletedUser){
-		isInAchat = true; 
-	}
-}
-
-if(isInAchat == false){
-	revert UserIsNotInAChat(); 
+modifier userIsInAChat(address user){
+if(!isInAChat[msg.sender][user]){
+	revert UserIsNotInAChat(user);	
 }
 _;
 }
@@ -68,87 +49,32 @@ _;
 
 /// @notice Creates a new chat with another user.
 /// @param newUser The address of the user to chat with.
-function createChat(address newUser) public isNotTheUserAlreadyInAChat(newUser){
+function createChat(address newUser) public userIsNotInAChat(newUser){
 if(newUser == msg.sender) {
 	revert CantCreateAChatWithHimself();
 }
 addressToChats[msg.sender].push(newUser); 
-}
+addressToChats[newUser].push(msg.sender);
+isInAChat[msg.sender][newUser] = true; 
+isInAChat[newUser][msg.sender] = true; 
 
-
-/// @notice Deletes a chat with another user.
-/// @param deletedUser The address of the user to remove from chats.
-function deleteChat(address deletedUser) public { 
-bool txWentTrue = false; 
-for(uint256 i = 0; i < addressToChats[msg.sender].length; i++){
-	if(keccak256(abi.encodePacked(addressToChats[msg.sender][i])) == keccak256(abi.encodePacked(deletedUser))){
-		uint256 getLastElement = addressToChats[msg.sender].length - 1; 
-		addressToChats[msg.sender][i] = addressToChats[msg.sender][getLastElement]; 
-		txWentTrue = true; 
-	} 
-
-addressToChats[msg.sender].pop(); 
-}
-
-if(!txWentTrue){
-	revert UserCantDeleteTheChat(); 
-}
+bytes memory chatKeyBytes = abi.encode(msg.sender, newUser);
+chatKey[msg.sender][newUser] = chatKeyBytes;
+chatKey[newUser][msg.sender] = chatKeyBytes;
 }
 
 
 /// @notice Sends a message to another user.
 /// @param user The recipient of the message.
 /// @param message The message content.
-function sendMessage(address user, string memory message) public isTheUserAlreadyInAChat(user){
-if(user == msg.sender) {
-	revert CantSendMessageToHimself();
-}  
-UserMessagges memory newUserMessage = UserMessagges({
+function sendMessage(address user, string memory message) public userIsInAChat(user){
+ChatMessage memory newChatMessage = ChatMessage({
+from: msg.sender,  
 message: message, 
-messageSender: msg.sender
+timestamp: block.timestamp
 }); 
-addressToUserMessagess[msg.sender][user].push(newUserMessage); 
-addressToChats[user].push(msg.sender); 
-addressToUserMessagess[user][msg.sender].push(newUserMessage); 
-}
-
-
-/// @notice Modifies an existing message in a chat.
-/// @param user The chat participant.
-/// @param messageToModify The original message.
-/// @param newMessage The new message content.
-function modifyMessage(address user, string memory messageToModify, string memory newMessage) public {
-bool txWentTrue = false; 
-for(uint256 i = 0; i < addressToUserMessagess[msg.sender][user].length; i++){
-	if(keccak256(abi.encodePacked(addressToUserMessagess[msg.sender][user][i].message)) == keccak256(abi.encodePacked(messageToModify))){
-		addressToUserMessagess[msg.sender][user][i].message = newMessage; 
-		txWentTrue = true; 
-	}
-}
-
-if(txWentTrue == false){
-	revert CantModifyTheMessage(); 
-}
-}
-
-
-/// @notice Deletes a message in a chat.
-/// @param user The chat participant.
-/// @param messageToDelete The message to delete.
-function deleteMessage(address user, string memory messageToDelete) public {
-bool txWentTrue = false; 
-for(uint256 i = 0; i < addressToUserMessagess[msg.sender][user].length; i++){
-	if(keccak256(abi.encodePacked(addressToUserMessagess[msg.sender][user][i].message)) == keccak256(abi.encodePacked(messageToDelete))){
-		uint256 lastIndex = addressToUserMessagess[msg.sender][user].length - 1; 
-		addressToUserMessagess[msg.sender][user][i] = addressToUserMessagess[msg.sender][user][lastIndex]; 
-		txWentTrue = true; 
-	}
-
-addressToUserMessagess[msg.sender][user].pop(); 
-}
-if(txWentTrue == false){
-	revert CantDeleteTheMessage(); 
-}
+bytes memory _chatKey = chatKey[msg.sender][user];
+messages[_chatKey].push(newChatMessage);
 }
 
 
@@ -160,12 +86,13 @@ return addressToChats[msg.sender];
 
 
 /// @notice Returns the chat between sender and another user.
-/// @param user The chat participant.
+/// @param _hash The bytes hash of the two user addresses.
 /// @return Array of user messages.
-function returnChat(address user) public isTheUserAlreadyInAChat(user) view returns(UserMessagges[] memory){
-if(user == msg.sender) {
-	revert CantSendMessageToHimself();
-} 
-return addressToUserMessagess[msg.sender][user]; 
+function returnChat(bytes memory _hash) public view returns(ChatMessage[] memory){
+return messages[_hash]; 
+}
+
+function returnChatKey(address userA, address userB) public view returns(bytes memory) {
+	return chatKey[userA][userB];
 }
 }
--- test/MessBlockChats.t.sol
+++ test/MessBlockChats.t.sol
@@ -3,18 +3,70 @@ pragma solidity ^0.8.13;
 
 import {Test} from "forge-std/Test.sol";
 import {MessBlock} from "../src/MessBlock.sol";
+import {MessBlockChats} from "../src/MessBlockChats.sol";
 
 contract MessBlockChatsTest is Test {
     
     address public david = makeAddr("david"); 
+    address public bob = makeAddr("bob"); 
     MessBlock public messBlock;
 
     function setUp() public {
         messBlock = new MessBlock(); 
     }
-
-    function test_createChat() public {
+    
+    //createChat tests
+    function test_canCreateChat() public {
         vm.startPrank(david);
+        messBlock.createChat(bob); 
+        address[] memory chats = messBlock.returnUserChats();
+        assertEq(chats[0], bob);
+		bytes memory chatHash_david_bob = messBlock.returnChatKey(david, bob);
+		assertNotEq(chatHash_david_bob.length, 0);
+		bytes memory chatHash_bob_david = messBlock.returnChatKey(bob, david);
+		assertNotEq(chatHash_bob_david.length, 0);
+        vm.stopPrank();
     }
 
+	function test_cantCreateAChat_userIsAlreadyInAChat() public {
+		vm.startPrank(david);
+		messBlock.createChat(bob); 
+		vm.expectRevert(abi.encodeWithSelector(MessBlockChats.UserIsAlreadyInAChat.selector, bob));
+		messBlock.createChat(bob); 
+		vm.stopPrank();
+	}
+
+    function test_cantCreateChat_CantCreateAChatWithHimself() public {
+        vm.startPrank(david);
+        vm.expectRevert(MessBlockChats.CantCreateAChatWithHimself.selector);
+        messBlock.createChat(david); 
+        vm.stopPrank();
+    }
+
+    //sendMessage tests
+    function test_canSendMessage() public {
+        vm.startPrank(david);
+        messBlock.createChat(bob); 
+        messBlock.sendMessage(bob, "Hello Bob!"); 
+        bytes memory chatHash = messBlock.returnChatKey(david, bob);
+        MessBlockChats.ChatMessage[] memory chat = messBlock.returnChat(chatHash);
+        assertEq(chat[0].from, david);
+        assertEq(chat[0].message, "Hello Bob!");
+        vm.stopPrank();
+
+        vm.startPrank(bob);
+        messBlock.sendMessage(david, "Hello David!"); 
+        chatHash = messBlock.returnChatKey(david, bob);
+        chat = messBlock.returnChat(chatHash);
+        assertEq(chat[1].from, bob);
+        assertEq(chat[1].message, "Hello David!");
+        vm.stopPrank();
+    }
+
+    function test_cantSendMessage_UserIsNotInAChat() public {
+        vm.startPrank(david);
+        vm.expectRevert(abi.encodeWithSelector(MessBlockChats.UserIsNotInAChat.selector, bob));
+        messBlock.sendMessage(bob, "Hello Bob!"); 
+        vm.stopPrank();
+    }
 }
