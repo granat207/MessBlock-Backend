ff1e3396fb6ceaf7a46a6f5acb1215245fecad98
Daniel_eth <danielvigo06@gmail.com>
2025-08-24 15:06:29 +0200

    Addedd groups tests

--- test/MessBlockChats.t.sol
+++ test/MessBlockChats.t.sol
@@ -5,10 +5,16 @@ import {Test} from "forge-std/Test.sol";
 import {MessBlock} from "../src/MessBlock.sol";
 
 contract MessBlockChatsTest is Test {
+    
+    address public david = makeAddr("david"); 
     MessBlock public messBlock;
 
     function setUp() public {
         messBlock = new MessBlock(); 
+    }
+
+    function test_createChat() public {
+        vm.startPrank(david);
     }
 
 }
